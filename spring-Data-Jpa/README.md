a3be7ff2a5e29827a31817f49b797c3930b05d91
Your Name <you@example.com>
2025-11-29 18:08:44 +0200

    add new project

---
+++ spring-Data-Jpa/README.md
@@ -0,0 +1,1 @@
+# spring-Data-Jpa
---
+++ spring-Data-Jpa/src/main/java/com/mariam/spring_JPA_hibernate/repository/StudentDao.java
@@ -0,0 +1,11 @@
+package com.mariam.spring_JPA_hibernate.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.mariam.spring_JPA_hibernate.entity.Student;
+
+
+public interface StudentDao extends JpaRepository<Student, Integer>{
+	
+	
+
+}
